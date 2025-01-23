73457c5706fc3bdc5dc3ea1693a674daafae8690
Aleksei Vlasov <alb883145@gmail.com>
2025-01-23 20:00:48 +0100

    Refactoring of product types

--- ProductAPI/Program.cs
+++ ProductAPI/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using ProductAPI;
 using ProductAPI.Data;
 using ProductAPI.Repository;
 
@@ -17,6 +18,8 @@ builder.Services.AddSwaggerGen(c =>
         Title = "ProductAPI",
         Version = "v1"
     });
+
+    c.SchemaFilter<EnumSchemaFilter>();
 });
 
 builder.Services.AddDbContext<ProductDbContext>(options =>
