9378404f1ce728d47bf09eecdb2bee55f58eccfe
Aleksei Vlasov <alb883145@gmail.com>
2025-03-29 18:03:38 +0100

    New test project with POM was added

---
+++ AutomationTests/TestProject/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TestFramework.Driver;
+using TestProject.Pages;
+
+namespace TestProject
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.UseWebDriverInitializer(BrowserType.Chrome);
+            services.AddScoped<IHomePage, HomePage>();
+            services.AddScoped<ICreateProductPage, CreateProductPage>();
+            services.AddScoped<IDriverFixture, DriverFixture>();
+            services.AddScoped<IBrowserDriver, BrowserDriver>();
+        }
+    }
+}
--- AutomationTests/TestProject/UnitTest1.cs
+++ AutomationTests/TestProject/UnitTest1.cs
@@ -1,11 +1,41 @@
-﻿namespace TestProject
+﻿using OpenQA.Selenium;
+using ProductAPI.Data;
+using TestFramework.Driver;
+using TestProject.Pages;
+
+namespace TestProject
 {
-    public class UnitTest1
+    public class UnitTest1 : IDisposable
     {
+        private readonly IHomePage homePage;
+        private readonly ICreateProductPage createProductPage;
+        IWebDriver driver;
+
+        public UnitTest1(IDriverFixture driverFixture, IHomePage homePage, ICreateProductPage createProductPage)
+        {
+            driver = driverFixture.Driver;
+            driver.Navigate().GoToUrl(new Uri("http://localhost:5001/"));
+            this.homePage = homePage;
+            this.createProductPage = createProductPage;
+        }
+
+        public void Dispose()
+        {
+            driver.Quit();
+        }
+
         [Fact]
         public void Test1()
         {
+            homePage.CreateProduct();
 
+            createProductPage.EnterProductDetails(new Product
+            {
+                Name = "AutoProduct",
+                Description = "AutoDescription",
+                Price = 444,
+                ProductType = ProductType.PERIPHARALS
+            });
         }
     }
 }
