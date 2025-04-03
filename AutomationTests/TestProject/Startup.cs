ca76a0609c8765e8e814cdd3e0962782b3f7b354
Aleksei Vlasov <alb883145@gmail.com>
2025-04-03 19:24:42 +0200

    Added appsettings file

--- AutomationTests/TestProject/Startup.cs
+++ AutomationTests/TestProject/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using TestFramework.Driver;
 using TestFramework.Extensions;
 using TestProject.Pages;
@@ -9,7 +10,12 @@ namespace TestProject
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.UseWebDriverInitializer(BrowserType.Chrome);
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json").Build();
+
+            var browserType = config.GetSection("BrowserType").Value;
+
+            services.UseWebDriverInitializer(Enum.Parse<BrowserType>(browserType));
             services.AddScoped<IHomePage, HomePage>();
             services.AddScoped<ICreateProductPage, CreateProductPage>();
             services.AddScoped<IDriverFixture, DriverFixture>();
