a13f4b1b0db9c67a30da8da1706dea5bd410f94c
Aleksei Vlasov <alb883145@gmail.com>
2025-05-21 21:11:22 +0200

    Added test with the NSwag generated code

--- AutomationTests/IntegrationTests/UnitTest1.cs
+++ AutomationTests/IntegrationTests/UnitTest1.cs
@@ -1,19 +1,20 @@
 ﻿using FluentAssertions;
 using Microsoft.AspNetCore.Mvc.Testing;
+using ProductAPI;
 
 namespace IntegrationTests;
 
-public class UnitTest1 : IClassFixture<WebApplicationFactory<ProductAPI.Program>>
+public class UnitTest1 : IClassFixture<WebApplicationFactory<Program>>
 {
-    public WebApplicationFactory<ProductAPI.Program> webApplicationFactory;
+    public WebApplicationFactory<Program> webApplicationFactory;
 
-    public UnitTest1(WebApplicationFactory<ProductAPI.Program> webApplicationFactory)
+    public UnitTest1(WebApplicationFactory<Program> webApplicationFactory)
     {
         this.webApplicationFactory = webApplicationFactory;
     }
 
     [Fact]
-    public void Test1()
+    public void TestWithHttpClient()
     {
         var client = new HttpClient();
         client.BaseAddress = new Uri("http://localhost:5000/");
@@ -35,5 +36,17 @@ public class UnitTest1 : IClassFixture<WebApplicationFactory<ProductAPI.Program>
         var result = product.Content.ReadAsStringAsync().Result;
 
         result.Should().Contain("Keyboard");
+    }
+
+    [Fact]
+    public async Task TestWithGeneratedCode()
+    {
+        var webClient = webApplicationFactory.CreateClient();
+
+        var product = new IntegrationTest.ProductAPI("https://localhost:44334/", webClient);
+
+        var results = await product.GetProductsAsync();
+
+        results.Select(x => x.Name == "Keyboard").Should().NotBeEmpty();
     }
 }
