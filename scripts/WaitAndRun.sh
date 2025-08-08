f04c884bf2a3b27fc639a0b6a646db1f39d31a2c
VlasovAlexei <86792437+VlasovAlexei@users.noreply.github.com>
2025-08-08 16:21:19 +0200

    Update WaitAndRun.sh

--- scripts/WaitAndRun.sh
+++ scripts/WaitAndRun.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env sh
-
 set -e
 set -x
 
-until [ ]; do
-    dotnet "$1" && break
-    sleep 1
-done 
+echo "Waiting for Selenium Grid..."
+until curl -sf http://selenium-hub:4444/wd/hub/status | grep -q '"ready": true'; do
+  sleep 5
+done
 
-until [ ]; do
-    sleep 30
-    curl -f "http://selenium-hub:4444/wd/hub/status" && break
-done 
-
-dotnet test --logger "console;verbosity=detailed"
+echo "Selenium is ready. Starting tests..."
+dotnet test TestProjectBDD.csproj --logger "console;verbosity=detailed"
